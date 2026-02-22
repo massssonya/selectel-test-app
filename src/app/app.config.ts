ffe0a2124fc1bae6f89c8c3a7c8b6359b30a65cc
massssonya <vladsozonov01@gmail.com>
2026-02-22 18:43:25 +0300

    fix: add environments for deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environments';
+
+const routerFeatures = environment.githubPages ? [withHashLocation()] : [];
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-	provideHttpClient()
-  ]
+    provideRouter(routes, ...routerFeatures),
+    provideHttpClient(),
+  ],
 };
---
+++ src/environments/environment.github.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  baseHref: '/selectel-test-app/',
+  githubPages: true
+};
