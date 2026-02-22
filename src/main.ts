b867b0c0bf02609c8201ab8e584d49439984fb09
massssonya <vladsozonov01@gmail.com>
2026-02-22 17:53:26 +0300

    fix: link using routerLink

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+const redirect = sessionStorage.getItem('redirect');
+if (redirect) {
+  sessionStorage.removeItem('redirect');
+  history.replaceState(null, '', redirect);
+}
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
